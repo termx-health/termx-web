c6c01c1af72a29b4e3b63f43f167d750101ad842
Roman Bondarev <roman.bondarev@kodality.com>
2022-04-28 13:26:44 +0300

    zorro in

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,8 +1,24 @@
 import {Component} from '@angular/core';
+import {MuiMenuItem} from '@kodality-health/marina-ui';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+  public menu: MuiMenuItem[] = [
+    {
+      icon: 'unordered-list',
+      label: 'CS list',
+      click: () => this.router.navigateByUrl('code-systems')
+    },
+    {
+      icon: 'plus',
+      label: 'CS add',
+      click: () => this.router.navigateByUrl('code-systems/add')
+    }
+  ];
+
+  public constructor(private router: Router) {}
 }
--- app/src/app/codesystem/code-system.module.ts
+++ app/src/app/codesystem/code-system.module.ts
@@ -7,6 +7,7 @@ import {FormsModule} from '@angular/forms';
 import {TranslateModule} from '@ngx-translate/core';
 import {CodeSystemLibModule} from 'terminology-lib/codesystem';
 import {MarinaUiModule} from '@kodality-health/marina-ui';
+import {MarinaUtilModule} from '@kodality-health/marina-util';
 
 
 export const CODE_SYSTEM_ROUTES: Routes = [
@@ -27,6 +28,7 @@ export const CODE_SYSTEM_ROUTES: Routes = [
     TranslateModule,
     CodeSystemLibModule,
     MarinaUiModule,
+    MarinaUtilModule,
   ]
 })
 export class CodeSystemModule {
