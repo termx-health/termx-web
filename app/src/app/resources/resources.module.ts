5aefa72eb1f2a42176ce0ee0587e837da0c64315
ViktorUst <viktor.ustits@kodality.com>
2022-05-17 18:00:13 +0300

    Resources tabs Menu and Add buttons on lists are still TODO

--- app/src/app/resources/resources.module.ts
+++ app/src/app/resources/resources.module.ts
@@ -1,13 +1,26 @@
 import {NgModule} from '@angular/core';
-import {Routes} from '@angular/router';
-import {CODE_SYSTEM_ROUTES, CodeSystemModule} from './codesystem/code-system.module';
-import {VALUE_SET_ROUTES, ValueSetModule} from './valueset/value-set.module';
+import {RouterModule, Routes} from '@angular/router';
+import {CODE_SYSTEM_ROUTES, CODE_SYSTEM_TAB_ROUTES, CodeSystemModule} from './codesystem/code-system.module';
+import {VALUE_SET_ROUTES, VALUE_SET_TAB_ROUTES, ValueSetModule} from './valueset/value-set.module';
 import {ConceptModule} from './concept/concept.module';
 import {DesignationModule} from './designation/designation.module';
+import {TabsComponent} from './tabs/tabs.component';
+import {MarinaUiModule} from '@kodality-health/marina-ui';
+
 
 export const RESOURCES_ROUTES: Routes = [
-  {path: 'code-systems', children: CODE_SYSTEM_ROUTES},
-  {path: 'value-sets', children: VALUE_SET_ROUTES},
+  {
+    path: 'tabs', component: TabsComponent, children: [
+      {path: 'code-systems', children: CODE_SYSTEM_TAB_ROUTES},
+      {path: 'value-sets', children: VALUE_SET_TAB_ROUTES},
+    ]
+  },
+  {
+    path: '', children: [
+      {path: 'code-systems', children: CODE_SYSTEM_ROUTES},
+      {path: 'value-sets', children: VALUE_SET_ROUTES},
+    ]
+  },
 ];
 
 const resourceModules = [
@@ -15,11 +28,14 @@ const resourceModules = [
   ValueSetModule,
   ConceptModule,
   DesignationModule
-]
+];
 
 @NgModule({
-  imports: [...resourceModules],
-  exports: [...resourceModules]
+  imports: [...resourceModules, MarinaUiModule, RouterModule],
+  exports: [...resourceModules],
+  declarations: [
+    TabsComponent
+  ]
 })
 export class ResourcesModule {
 }
---
+++ app/src/app/resources/tabs/tabs.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'twa-tabs',
+  templateUrl: './tabs.component.html',
+})
+export class TabsComponent implements OnInit {
+
+  public constructor() { }
+
+  public ngOnInit(): void {
+  }
+
+}
