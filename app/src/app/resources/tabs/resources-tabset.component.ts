7168ad6bfe627013f44590ebb38708eb78dab0f9
ViktorUst <viktor.ustits@kodality.com>
2022-05-18 16:17:46 +0300

    extracted badge style

--- app/src/app/resources/tabs/resources-tabset.component.ts
+++ app/src/app/resources/tabs/resources-tabset.component.ts
@@ -5,5 +5,6 @@ import {Component} from '@angular/core';
   templateUrl: './resources-tabset.component.html',
 })
 export class ResourcesTabsetComponent {
+  public badgeStyle = { backgroundColor: '#fff', color: '#999', boxShadow: '0 0 0 1px #d9d9d9 inset' };
 
 }
