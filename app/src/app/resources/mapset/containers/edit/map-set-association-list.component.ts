cd7e3583ecc29e6f88dd7f7890983b8248001c32
Viktor Ustitš <viktor.ustits@kodality.com>
2022-05-23 10:36:32 +0000

    map-set-association-list

---
+++ app/src/app/resources/mapset/containers/edit/map-set-association-list.component.ts
@@ -0,0 +1,39 @@
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
+import {MapSetService} from '../../services/map-set-service';
+import {collect} from '@kodality-web/core-util';
+import {MapSetAssociation} from 'terminology-lib/resources';
+
+@Component({
+  selector: 'twa-map-set-association-list',
+  templateUrl: './map-set-association-list.component.html',
+})
+export class MapSetAssociationListComponent implements OnChanges {
+  @Input() public mapSetId?: string;
+
+  public data: {[id: string]: MapSetAssociation[]} = {};
+  public loading: boolean = false;
+
+  public constructor(private mapSetService: MapSetService) { }
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (changes['mapSetId']) {
+      this.loadAssociations();
+    }
+  }
+
+  public loadAssociations(): void {
+    this.loading = true;
+    this.mapSetService.searchAssociations(this.mapSetId!)
+      .subscribe(a => this.data = collect(a.data, a => a.source?.codeSystem!))
+      .add(() => this.loading = false);
+  }
+
+  public activateAssociation(): void {
+// TODO when backend supports
+  }
+
+  public retireAssociation(): void {
+// TODO when backend supports
+  }
+
+}
---
+++ lib/src/resources/mapset/model/map-set-association-search-params.ts
@@ -0,0 +1,5 @@
+import {QueryParams} from '@kodality-web/core-util';
+
+export class MapSetAssociationSearchParams extends QueryParams {
+
+}
