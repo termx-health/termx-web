4f9cdbc0460212ceb3cdcffc47886cda5f72d3bb
ViktorUst <viktor.ustits@kodality.com>
2022-05-23 13:32:42 +0300

    Added association params to public api

--- lib/src/resources/mapset/public-api.ts
+++ lib/src/resources/mapset/public-api.ts
@@ -3,5 +3,6 @@ export * from './model/map-set-association';
 export * from './model/map-set-entity';
 export * from './model/map-set-search-params';
 export * from './model/map-set-version';
+export * from './model/map-set-association-search-params';
 export * from './services/map-set-lib.service';
 export * from './map-set-lib.module';
