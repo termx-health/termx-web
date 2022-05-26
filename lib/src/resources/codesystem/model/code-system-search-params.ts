bceca83445023248b2ecd636074ccb0dbda2a3be
Roman Bondarev <roman.bondarev@kodality.com>
2022-05-26 18:17:22 +0300

    add resource list's sorting

--- lib/src/resources/codesystem/model/code-system-search-params.ts
+++ lib/src/resources/codesystem/model/code-system-search-params.ts
@@ -1,8 +1,31 @@
 import {QueryParams} from '@kodality-web/core-util';
 
 export class CodeSystemSearchParams extends QueryParams {
+  public id?: string;
+  public idContains?: string;
+  public uri?: string;
+  public uriContains?: string;
   public name?: string;
+  public nameContains?: string;
+  public description?: string;
+  public descriptionContains?: string;
+
+  public text?: string;
   public textContains?: string;
+
+  public conceptCode?: string;
+  public conceptCodeSystemVersion?: string;
+  public conceptsDecorated?: boolean;
+
+  public versionId?: number;
+  public versionVersion?: string;
+  public versionReleaseDateGe?: Date;
+  public versionExpirationDateLe?: Date;
   public versionsDecorated?: boolean;
+
   public propertiesDecorated?: boolean;
+
+  public codeSystemEntityVersionId?: number
+
+  public lang?: string;
 }
