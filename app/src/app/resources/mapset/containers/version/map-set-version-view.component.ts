8e05c0cb29549bebb5e219e753f25a13e4cc082a
ViktorUst <viktor.ustits@kodality.com>
2022-06-14 12:56:37 +0300

    retired status now views version, active status can edit expiration date, removed back buttons and renamed modal back buttons to cancel

--- app/src/app/resources/mapset/containers/version/map-set-version-view.component.ts
+++ app/src/app/resources/mapset/containers/version/map-set-version-view.component.ts
@@ -1,16 +1,21 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {MapSetVersion} from 'terminology-lib/resources';
 import {ActivatedRoute} from '@angular/router';
 import {MapSetService} from '../../services/map-set-service';
 import {Location} from '@angular/common';
+import {validateForm} from '@kodality-web/core-util';
+import {NgForm} from '@angular/forms';
 
 
 @Component({
   templateUrl: './map-set-version-view.component.html',
 })
 export class MapSetVersionViewComponent implements OnInit {
+  public mapSetId?: string | null;
   public version?: MapSetVersion;
   public loading = false;
+
+  @ViewChild("form") public form?: NgForm;
 
   public constructor(
     private mapSetService: MapSetService,
@@ -19,9 +24,9 @@ export class MapSetVersionViewComponent implements OnInit {
   ) {}
 
   public ngOnInit(): void {
-    const mapSetId = this.route.snapshot.paramMap.get('id');
+    this.mapSetId = this.route.snapshot.paramMap.get('id');
     const mapSetVersion = this.route.snapshot.paramMap.get('version');
-    this.loadVersion(mapSetId!, mapSetVersion!);
+    this.loadVersion(this.mapSetId!, mapSetVersion!);
   }
 
   private loadVersion(id: string, version: string): void {
@@ -29,7 +34,13 @@ export class MapSetVersionViewComponent implements OnInit {
     this.mapSetService.loadVersion(id, version).subscribe(v => this.version = v).add(() => this.loading = false);
   }
 
-  public back(): void {
-    this.location.back();
+  public save(): void {
+    if (!validateForm(this.form)) {
+      return;
+    }
+    this.loading = true;
+    this.mapSetService.saveVersion(this.mapSetId!, this.version!)
+      .subscribe(() => this.location.back())
+      .add(() => this.loading = false);
   }
 }
