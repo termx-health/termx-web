6175560195700f5f5fc473086afcbf2adfc1d50b
ViktorUst <viktor.ustits@kodality.com>
2022-06-20 15:58:39 +0300

    naming-system-list Search doesn't work yet due to backend. Add button and activate/retire don't work either.

---
+++ lib/src/resources/namingsystem/naming-system-lib.module.ts
@@ -0,0 +1,11 @@
+import {NgModule} from '@angular/core';
+import {NamingSystemLibService} from './services/naming-system-lib.service';
+
+@NgModule({
+  providers: [
+    NamingSystemLibService
+  ]
+})
+
+export class NamingSystemLibModule {
+}
