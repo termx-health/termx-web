00b53b6d1d06f3362ce0ec5e2912444a8a0d72a6
Roman Bondarev <roman.bondarev@kodality.com>
2022-06-30 12:38:29 +0300

    clean *-select & *-search components

--- lib/src/resources/codesystem/containers/concept-search.component.ts
+++ lib/src/resources/codesystem/containers/concept-search.component.ts
@@ -20,10 +20,9 @@ export class ConceptSearchComponent implements OnInit, OnChanges, ControlValueAc
   @Input() public entityVersionStatus?: string;
   @Input() @BooleanInput() public valuePrimitive: string | boolean = false;
 
-  public searchUpdate = new Subject<string>();
-
   public data: {[id: string]: CodeSystemConcept} = {};
   public value?: number;
+  public searchUpdate = new Subject<string>();
   private loading: {[key: string]: boolean} = {};
 
   public onChange = (x: any) => x;
@@ -46,7 +45,8 @@ export class ConceptSearchComponent implements OnInit, OnChanges, ControlValueAc
       || changes["codeSystemVersion"]
       || changes["codeSystemVersionReleaseDateLe"]
       || changes["codeSystemVersionExpirationDateGe"]
-      || changes["entityVersionStatus"]) {
+      || changes["entityVersionStatus"]
+    ) {
       this.data = {};
       this.searchConcepts().subscribe(data => this.data = data);
     }
@@ -56,10 +56,11 @@ export class ConceptSearchComponent implements OnInit, OnChanges, ControlValueAc
     this.searchUpdate.next(text);
   }
 
-  public searchConcepts(text?: string): Observable<{[id: string]: CodeSystemConcept}> {
+  private searchConcepts(text?: string): Observable<{[id: string]: CodeSystemConcept}> {
     if ((!text || text.length < 1) && !this.codeSystem && !this.codeSystemVersion) {
       return of(this.data);
     }
+
     const q = new ConceptSearchParams();
     q.codeContains = text;
     q.codeSystem = this.codeSystem;
@@ -68,6 +69,7 @@ export class ConceptSearchComponent implements OnInit, OnChanges, ControlValueAc
     q.codeSystemVersionExpirationDateGe = this.codeSystemVersionExpirationDateGe;
     q.codeSystemEntityStatus = this.entityVersionStatus;
     q.limit = 10_000;
+
     this.loading['search'] = true;
     return this.conceptService.search(q).pipe(
       map(ca => ({...this.data, ...group(ca.data, c => c.id!)})),
@@ -82,6 +84,7 @@ export class ConceptSearchComponent implements OnInit, OnChanges, ControlValueAc
       this.conceptService.load(id).subscribe(c => this.data = {...(this.data || {}), [c.id!]: c}).add(() => this.loading['load'] = false);
     }
   }
+
 
   public writeValue(obj: CodeSystemConcept | number): void {
     this.value = (typeof obj === 'object' ? obj?.id : obj);
@@ -104,8 +107,8 @@ export class ConceptSearchComponent implements OnInit, OnChanges, ControlValueAc
     this.onTouched = fn;
   }
 
+
   public get isLoading(): boolean {
     return Object.values(this.loading).some(Boolean);
   }
-
 }
