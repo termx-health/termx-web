654a573b5b6c51c20a9f868bfb93b067d5d17c80
Juri Antonov <juri.antonov@kodality.com>
2022-11-17 11:03:12 +0200

    increase json limit

--- fsh-chef/index.js
+++ fsh-chef/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-app.use(express.json())
+app.use(express.json({limit: '100mb'}));
 const port = 3000;
 
 const {sushiClient} = require('fsh-sushi');
