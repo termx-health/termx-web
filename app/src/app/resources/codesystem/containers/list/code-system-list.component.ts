8e68bbcdf95624d6192569bf5bc447a779615d41
Roman Bondarev <roman.bondarev@kodality.com>
2022-12-21 12:14:09 +0200

    codesystem list state search marina ui fix

--- app/src/app/resources/codesystem/containers/list/code-system-list.component.ts
+++ app/src/app/resources/codesystem/containers/list/code-system-list.component.ts
@@ -1,16 +1,18 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {copyDeep, SearchResult} from '@kodality-web/core-util';
+import {ComponentStateStore, copyDeep, DestroyService, QueryParams, SearchResult} from '@kodality-web/core-util';
 import {CodeSystem, CodeSystemSearchParams} from 'terminology-lib/resources';
 import {CodeSystemService} from '../../services/code-system.service';
 import {TranslateService} from '@ngx-translate/core';
-import {debounceTime, distinctUntilChanged, finalize, Observable, Subject, switchMap} from 'rxjs';
+import {debounceTime, distinctUntilChanged, finalize, Observable, Subject, switchMap, tap} from 'rxjs';
 
 @Component({
   selector: 'twa-code-system-list',
-  templateUrl: 'code-system-list.component.html'
+  templateUrl: 'code-system-list.component.html',
+  providers: [DestroyService]
 })
 export class CodeSystemListComponent implements OnInit {
   @Input() public dev: boolean = false;
+  protected readonly STORE_KEY = 'code-system-list';
 
   public query = new CodeSystemSearchParams();
   public searchInput?: string;
@@ -20,14 +22,22 @@ export class CodeSystemListComponent implements OnInit {
 
   public constructor(
     private codeSystemService: CodeSystemService,
-    private translateService: TranslateService
+    private translateService: TranslateService,
+    private stateStore: ComponentStateStore,
   ) {}
 
   public ngOnInit(): void {
+    const state = this.stateStore.pop(this.STORE_KEY);
+    if (state) {
+      this.query = Object.assign(new QueryParams(), state.query);
+      this.searchInput = this.query.textContains;
+    }
+
     this.loadData();
     this.searchUpdate.pipe(
       debounceTime(250),
       distinctUntilChanged(),
+      tap(() => this.query.offset = 0),
       switchMap(() => this.search()),
     ).subscribe(data => this.searchResult = data);
   }
@@ -38,6 +48,8 @@ export class CodeSystemListComponent implements OnInit {
     q.lang = this.translateService.currentLang;
     q.versionsDecorated = true;
     q.textContains = this.searchInput;
+    this.stateStore.put(this.STORE_KEY, {query: q});
+
     this.loading = true;
     return this.codeSystemService.search(q).pipe(finalize(() => this.loading = false));
   }
