eab64bad37df982c9400538be39b02d818fbe6ef
Marina Ivanova <marina.ivanova@kodality.com>
2023-01-06 12:42:19 +0200

    cs delete fix

--- app/src/app/resources/codesystem/containers/list/code-system-list.component.ts
+++ app/src/app/resources/codesystem/containers/list/code-system-list.component.ts
@@ -63,6 +63,7 @@ export class CodeSystemListComponent implements OnInit {
   }
 
   public deleteCodeSystem(codeSystemId: string): void {
-    this.codeSystemService.delete(codeSystemId).subscribe(() => this.loadData());
+    this.loading = true;
+    this.codeSystemService.delete(codeSystemId).subscribe(() => this.loadData()).add(() => this.loading = false);
   }
 }
