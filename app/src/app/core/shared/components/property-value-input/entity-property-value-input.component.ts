2db9323031174166facb13372cc47890646fd441
Roman Bondarev <roman.bondarev@kodality.com>
2023-05-10 16:48:20 +0300

    EntityPropertyValueInputCacheService

--- app/src/app/core/shared/components/property-value-input/entity-property-value-input.component.ts
+++ app/src/app/core/shared/components/property-value-input/entity-property-value-input.component.ts
@@ -1,13 +1,32 @@
-import {Component, forwardRef, Input, OnChanges, SimpleChanges, ViewChild} from '@angular/core';
+import {Component, forwardRef, Injectable, Input, OnChanges, SimpleChanges, ViewChild} from '@angular/core';
 import {ControlValueAccessor, NG_VALUE_ACCESSOR, NgForm} from '@angular/forms';
-import {BooleanInput, DestroyService, isDefined, validateForm} from '@kodality-web/core-util';
+import {BooleanInput, DestroyService, HttpCacheService, isDefined, validateForm} from '@kodality-web/core-util';
 import {CodeSystemLibService, EntityProperty} from 'term-web/resources/_lib';
+import {Observable} from 'rxjs';
 
+
+@Injectable({providedIn: 'root'})
+class EntityPropertyValueInputCacheService {
+  public constructor(
+    private codeSystemService: CodeSystemLibService,
+    private cachedService: HttpCacheService
+  ) { }
+
+  public loadEntityProperty(codeSystemId: string, entityPropertyId: number): Observable<EntityProperty> {
+    return this.cachedService.getCachedResponse(`${codeSystemId}-${entityPropertyId}`,
+      this.codeSystemService.loadEntityProperty(codeSystemId, entityPropertyId)
+    );
+  }
+}
 
 @Component({
   selector: 'tw-property-value-input',
   templateUrl: './entity-property-value-input.component.html',
-  providers: [{provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => EntityPropertyValueInputComponent), multi: true}, DestroyService]
+  providers: [
+    {provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => EntityPropertyValueInputComponent), multi: true},
+    EntityPropertyValueInputCacheService,
+    DestroyService
+  ]
 })
 export class EntityPropertyValueInputComponent implements OnChanges, ControlValueAccessor {
   @Input() @BooleanInput() public viewMode: boolean | string = false;
@@ -24,7 +43,9 @@ export class EntityPropertyValueInputComponent implements OnChanges, ControlValu
   public onChange = (x: any): void => x;
   public onTouched = (x: any): void => x;
 
-  public constructor(private codeSystemService : CodeSystemLibService) {}
+  public constructor(
+    private codeSystemService: EntityPropertyValueInputCacheService
+  ) {}
 
   public ngOnChanges(changes: SimpleChanges): void {
     if ((changes['propertyId'] || changes['codeSystem']) && this.propertyId && this.codeSystem) {
