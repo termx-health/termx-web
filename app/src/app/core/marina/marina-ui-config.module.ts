66d3d3f18f522cb6203b11b65ff30fc3eb8eaa9a
Marina Ivanova <marina.ivanova@kodality.com>
2023-05-22 14:50:41 +0300

    multi lang input in lt

--- app/src/app/core/marina/marina-ui-config.module.ts
+++ app/src/app/core/marina/marina-ui-config.module.ts
@@ -12,11 +12,10 @@ export function MarinaUiConfigFactory(external: MuiConfig): MuiConfig {
   return {
     multiLanguageInput: {
       languages: [
-        {code: 'en', names: {'en': 'English', 'et': 'Inglise', 'ru': 'Английский', 'uz-LATN': 'Ingliz', 'uz-CYRL': 'Инглиз'}},
-        {code: 'et', names: {'en': 'Estonian', 'et': 'Eesti'}},
-        {code: 'ru', names: {'en': 'Russian', 'et': 'Vene', 'ru': 'Русский', 'uz-LATN': 'Rus', 'uz-CYRL': 'Рус'}},
-        {code: 'uz-CYRL', names: {'en': 'Uzbek cyrl', 'ru': 'Узбекский латиница', 'uz-LATN': 'Ўзбекча', 'uz-CYRL': 'Ўзбекча'}},
-        {code: 'uz-LATN', names: {'en': 'Uzbek latn', 'ru': 'Узбекский кириллица', 'uz-LATN': 'Oʻzbekcha', 'uz-CYRL': 'Oʻzbekcha'}},
+        {code: 'en', names: {'en': 'English', 'et': 'Inglise', 'ru': 'Английский', 'lt': 'Anglų'}},
+        {code: 'et', names: {'en': 'Estonian', 'et': 'Eesti', 'ru': 'Эстонский', 'lt': 'Estų'}},
+        {code: 'ru', names: {'en': 'Russian', 'et': 'Vene', 'ru': 'Русский', 'lt': 'Rusų'}},
+        {code: 'lt', names: {'en': 'Lithuanian', 'et': 'Leedu', 'ru': 'Литовский', 'lt': 'Lietuvių'}},
       ],
       requiredLanguages: ['en']
     },
@@ -26,7 +25,7 @@ export function MarinaUiConfigFactory(external: MuiConfig): MuiConfig {
     supportedLangs: {
       en: {label: 'EN English'},
       et: {label: 'ET Eesti'},
-      lt: {label: 'LT'}
+      lt: {label: 'LT Lietuvių'}
     },
     ...external
   };
