f8fb51ed649fab26d5959f06447c4e82204e312d
Marina Ivanova <marina.ivanova@kodality.com>
2023-07-12 14:57:12 +0300

    OP-5291

--- app/src/app/space/_lib/model/terminology-server.ts
+++ app/src/app/space/_lib/model/terminology-server.ts
@@ -4,6 +4,7 @@ export class TerminologyServer {
   public id?: number;
   public code?: string;
   public names?: LocalizedName;
+  public kind?: string[];
   public rootUrl?: string;
   public active?: boolean;
   public currentInstallation?: boolean;
