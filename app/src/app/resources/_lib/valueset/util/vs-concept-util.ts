2385ad8ef9630e18a22886f273fb525ec295ee6d
Marina Ivanova <marina.ivanova@kodality.com>
2023-08-02 09:55:55 +0300

    vs concept display fix

--- app/src/app/resources/_lib/valueset/util/vs-concept-util.ts
+++ app/src/app/resources/_lib/valueset/util/vs-concept-util.ts
@@ -3,7 +3,8 @@ import {ConceptUtil, ValueSetVersionConcept} from 'term-web/resources/_lib';
 export class VsConceptUtil {
 
   public static getDisplay(concept: ValueSetVersionConcept, lang: string): string {
-    const d = concept?.additionalDesignations?.find(ad => ad?.language === lang)?.name;
-    return d || ConceptUtil.getDisplay(concept.concept, lang);
+    const d = concept?.display?.language === lang ? concept.display.name : undefined;
+    const ad = concept?.additionalDesignations?.find(ad => ad?.language === lang)?.name;
+    return d || ad || ConceptUtil.getDisplay(concept.concept, lang);
   }
 }
