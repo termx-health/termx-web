323291bb62e506ffcfe9e3fab0af0d534571b129
Marina Ivanova <marina.ivanova@kodality.com>
2023-09-19 14:53:45 +0300

    OP-5424

--- app/src/app/space/containers/space/space-dashboard.component.ts
+++ app/src/app/space/containers/space/space-dashboard.component.ts
@@ -1,11 +1,23 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {SpaceContextComponent} from 'term-web/core/context/space-context.component';
+import {ComponentStateStore} from '@kodality-web/core-util';
 
 @Component({
   templateUrl: './space-dashboard.component.html',
 })
-export class SpaceDashboardComponent {
-  public selectedResourceType: string = 'code-system';
+export class SpaceDashboardComponent implements OnInit {
+  protected readonly STORE_KEY = 'space-dashboard';
 
-  public constructor(public spaceContext: SpaceContextComponent) {}
+  public selectedResourceType: 'code-system' | 'value-set' | 'map-set';
+  public constructor(public spaceContext: SpaceContextComponent, private stateStore: ComponentStateStore) {}
+
+  public ngOnInit(): void {
+    this.selectedResourceType = this.stateStore.pop(this.STORE_KEY) || 'code-system';
+
+  }
+
+  public selectResourceType(type: 'code-system' | 'value-set' | 'map-set'): void {
+    this.selectedResourceType = type;
+    this.stateStore.put(this.STORE_KEY, type);
+  }
 }
