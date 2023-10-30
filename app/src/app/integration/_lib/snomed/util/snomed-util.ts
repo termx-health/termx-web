c2abe5a277d295b23c116468ccb8ac7493dd8eac
Marina Ivanova <marina.ivanova@kodality.com>
2023-10-30 14:31:10 +0200

    OP-5494

--- app/src/app/integration/_lib/snomed/util/snomed-util.ts
+++ app/src/app/integration/_lib/snomed/util/snomed-util.ts
@@ -3,8 +3,12 @@ import {SnomedConcept} from '../model/concept/snomed-concept';
 export class SnomedUtil {
 
   public static formatConcept(concept: SnomedConcept, type: 'pt' | 'fsn' = 'pt'): string {
-    const etDescription = concept && concept.descriptions && concept.descriptions.find(d => d.lang === 'et');
-    const description = etDescription ? etDescription.term : (concept && concept.pt && concept.pt.lang === 'et' ? concept.pt.term : concept && concept[type]?.term);
-    return description ? description : concept.conceptId;
+    if (type == 'pt') {
+      return concept.pt?.term || concept.fsn?.term || concept.conceptId;
+    }
+    if (type == 'fsn') {
+      return concept.fsn?.term || concept.pt?.term || concept.conceptId;
+    }
+    return concept.conceptId;
   }
 }
