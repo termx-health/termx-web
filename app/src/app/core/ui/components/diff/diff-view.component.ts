898e158c4bb5f9c983b1f665ae9d3ad18d94e5b6
Roman Bondarev <roman.bondarev@kodality.com>
2023-11-02 16:31:16 +0200

    diff view

--- app/src/app/core/ui/components/diff/diff-view.component.ts
+++ app/src/app/core/ui/components/diff/diff-view.component.ts
@@ -1,8 +1,8 @@
-import {Component, Input} from '@angular/core';
+import {Component, ElementRef, Input, OnChanges, ViewChild} from '@angular/core';
 import {OutputFormatType} from 'diff2html/lib/types';
 import {createPatch} from 'diff';
 import {html} from 'diff2html';
-import {BooleanInput} from '@kodality-web/core-util';
+import {BooleanInput, isNil} from '@kodality-web/core-util';
 
 const SIDE_BY_SIDE_TEMPLATE = `
   <div id="{{fileHtmlId}}" class="d2h-file-wrapper" data-lang="{{file.language}}">
@@ -47,15 +47,8 @@ const LINE_BY_LINE_TEMPLATE = `
   selector: 'tw-diff-view',
   template: `
     <div
-        *ngIf="!value"
-        [innerHTML]="src | apply: generateDiff: tgt: display: full"
-        style="background-color: var(--d2h-bg-color)"
-    ></div>
-
-    <div
-        *ngIf="value"
-        class="tw-diff-view--single"
-        [innerHTML]="value | apply: displayValue"
+        #el
+        [class.tw-diff-view--single]="value"
         style="background-color: var(--d2h-bg-color)"
     ></div>
   `,
@@ -79,15 +72,39 @@ const LINE_BY_LINE_TEMPLATE = `
     }
   `]
 })
-export class DiffViewComponent {
+export class DiffViewComponent implements OnChanges {
   @Input() public value: string;
   @Input() public src: string;
   @Input() public tgt: string;
-
   @Input() public display: OutputFormatType | string = 'line-by-line';
   @Input() @BooleanInput() public full: boolean | string;
+  @Input() public numberOffset: number = 1;
 
-  protected generateDiff = (source: string, target: string, viewMode: OutputFormatType, full: boolean): string => {
+  private _el: HTMLDivElement;
+
+  @ViewChild('el')
+  public set el(e: ElementRef<HTMLDivElement>) {
+    this._el = e.nativeElement;
+    this.generate();
+  };
+
+  public ngOnChanges(): void {
+    this.generate();
+  }
+
+
+  private generate(): void {
+    if (isNil(this.el)) {
+      return;
+    }
+    if (this.value) {
+      this._el.innerHTML = this.displayValue(this.value);
+    } else {
+      this._el.innerHTML = this.generateDiff(this.src, this.tgt, this.display as OutputFormatType, this.full as boolean);
+    }
+  }
+
+  private generateDiff = (source: string, target: string, viewMode: OutputFormatType, full: boolean): string => {
     source ||= '';
     target ||= '';
 
@@ -106,7 +123,7 @@ export class DiffViewComponent {
     });
   };
 
-  protected displayValue = (value: string): string => {
+  private displayValue = (value: string): string => {
     const lines = value.split('\n');
 
     const patch = '' +
@@ -114,7 +131,7 @@ export class DiffViewComponent {
       '===================================================================\n' +
       '--- \t\n' +
       '+++ \t\n' +
-      `@@ -0,0 +1,${lines.length} @@\n` +
+      `@@ -0,0 +${this.numberOffset},${lines.length} @@\n` +
       lines.map(l => ' ' + l).join('\n') +
       '\\ No newline at end of file`';
 
