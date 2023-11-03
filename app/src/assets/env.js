0a882e5f4c8b97b43d8d2f238e8486997eab37ce
Daniel Dubrovski <daniel@kodality.com>
2023-11-03 09:01:24 +0200

    env

--- app/src/assets/env.js
+++ app/src/assets/env.js
@@ -1,5 +1,11 @@
 twConfig = {
   "oauthIssuer": "${OAUTH_ISSUER}",
   "oauthClientId": "${OAUTH_CLIENT_ID}",
-  "termxApi": "${TERMX_API}"
+  "termxApi": "${TERMX_API}",
+  "swaggerUrl": "${SWAGGER_URL}",
+  "chefUrl": "${CHEF_URL}",
+  "plantUmlUrl": "${PLANT_UML_URL}",
+  "fmlEditor": "${FML_EDITOR}",
+  "snowstormUrl": "${SNOWSTORM_URL}",
+  "snowstormDailyBuildUrl": "${SNOWSTORM_DAILY_BUILD_URL}",
 };
