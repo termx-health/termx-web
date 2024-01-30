dbc3ceec92282baa617c5b6ccabc390e39d72873
Marina Ivanova <marina.ivanova@kodality.com>
2024-01-30 13:37:52 +0200

    snomed concept review task

--- app/src/app/resources/resource/components/resource-tasks-widget.component.ts
+++ app/src/app/resources/resource/components/resource-tasks-widget.component.ts
@@ -1,10 +1,11 @@
 import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
-import {BooleanInput, isDefined} from '@kodality-web/core-util';
+import {BooleanInput, isDefined, LoadingManager} from '@kodality-web/core-util';
 import {Task, TaskLibService} from 'term-web/task/_lib';
 import {Router} from '@angular/router';
 import {environment} from 'environments/environment';
 import {ReleaseLibService} from 'term-web/sys/_lib';
 import {map, Observable} from 'rxjs';
+import {SnomedTranslationLibService} from 'term-web/integration/_lib';
 
 @Component({
   selector: 'tw-resource-tasks-widget',
@@ -14,11 +15,13 @@ export class ResourceTasksWidgetComponent implements OnChanges {
   @Input() public resourceId: string;
   @Input() public taskFilters: {statuses?: string[]};
   @Input() public resourceType: 'CodeSystem' | 'ValueSet' | 'MapSet' | 'CodeSystemVersion' | 'ValueSetVersion' | 'MapSetVersion' | 'CodeSystemEntityVersion' |
-    'Release';
+    'Release' | 'SnomedConcept';
   @Input() public displayType: 'full' | 'content' = 'full';
   @Input() @BooleanInput() public openInNewTab: boolean | string = false;
 
   protected tasks: Task[];
+
+  protected loader = new LoadingManager();
 
   private resourceTypeMap: {[key: string]: string} = {
     'CodeSystem': 'code-system',
@@ -30,7 +33,12 @@ export class ResourceTasksWidgetComponent implements OnChanges {
     'CodeSystemEntityVersion': 'concept-version'
   };
 
-  public constructor(private taskLibService: TaskLibService, private router: Router, private releaseService: ReleaseLibService) {}
+  public constructor(
+    private taskLibService: TaskLibService,
+    private router: Router,
+    private snomedService: SnomedTranslationLibService,
+    private releaseService: ReleaseLibService
+  ) {}
 
   public ngOnChanges(changes: SimpleChanges): void {
     if ((changes['resourceId'] || changes['resourceType']) && isDefined(this.resourceId) && isDefined(this.resourceType)) {
@@ -52,12 +60,19 @@ export class ResourceTasksWidgetComponent implements OnChanges {
 
   public loadTasks(): void {
     if (this.resourceType === 'Release') {
-      this.releaseService.loadResources(Number(this.resourceId)).subscribe(resources => {
+      this.loader.wrap('load', this.releaseService.loadResources(Number(this.resourceId))).subscribe(resources => {
         this.tasks = [];
-        resources.forEach(r => this.loadResourceTasks(r.resourceId, this.resourceTypeMap[r.resourceType]).subscribe(t => this.tasks = [...this.tasks, ...t]));
+        resources.forEach(r => this.loader.wrap('load', this.loadResourceTasks(r.resourceId, this.resourceTypeMap[r.resourceType])).subscribe(t => this.tasks = [...this.tasks, ...t]));
       });
     }
-    this.loadResourceTasks(this.resourceId, this.resourceTypeMap[this.resourceType]).subscribe(t => this.tasks = t);
+    if (this.resourceType === 'SnomedConcept') {
+      this.loader.wrap('load', this.snomedService.loadConceptTranslations(this.resourceId)).subscribe(translations => {
+        this.tasks = [];
+        this.loader.wrap('load', this.loadResourceTasks(this.resourceId, 'snomed-concept')).subscribe(t => this.tasks = [...this.tasks, ...t]);
+        translations.forEach(t => this.loadResourceTasks(String(t.id), 'snomed-translation').subscribe(t => this.tasks = [...this.tasks, ...t]));
+      });
+    }
+    this.loader.wrap('load', this.loadResourceTasks(this.resourceId, this.resourceTypeMap[this.resourceType])).subscribe(t => this.tasks = t);
   }
 
   private loadResourceTasks(id: string, type: string): Observable<Task[]> {
