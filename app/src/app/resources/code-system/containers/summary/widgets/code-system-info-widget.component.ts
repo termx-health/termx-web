4993f139b5bd5dfc2a956a3f0e10efe79c51e100
Marina Ivanova <marina.ivanova@kodality.com>
2024-08-01 11:29:57 +0300

    cs/vs telecoms fix

--- app/src/app/resources/code-system/containers/summary/widgets/code-system-info-widget.component.ts
+++ app/src/app/resources/code-system/containers/summary/widgets/code-system-info-widget.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input} from '@angular/core';
+import {group, collect} from '@kodality-web/core-util';
 import {CodeSystem, Telecom} from 'app/src/app/resources/_lib';
 
 @Component({
@@ -8,7 +9,8 @@ import {CodeSystem, Telecom} from 'app/src/app/resources/_lib';
 export class CodeSystemInfoWidgetComponent {
   @Input() public codeSystem: CodeSystem;
 
-  protected getTelecoms = (cs: CodeSystem): Telecom[] => {
-    return cs.contacts?.flatMap(c => c.telecoms);
+  protected getTelecoms = (cs: CodeSystem): {[dType: string]: Telecom[]} => {
+    return collect(cs.contacts?.flatMap(c => c.telecoms), t => t.system);
   };
+  protected readonly group = group;
 }
