61ca4c8ef520b6a4596a40521ed5c77ba110199d
Roman Bondarev <roman.bondarev@kodality.com>
2024-08-07 09:11:12 +0300

    fix: StructureDefinition type using url instead of code

--- app/src/app/modeler/structure-definition/containers/structure-definition-edit.component.ts
+++ app/src/app/modeler/structure-definition/containers/structure-definition-edit.component.ts
@@ -204,7 +204,7 @@ export class StructureDefinitionEditComponent implements OnInit {
     structureDefinition.resourceType ||= 'StructureDefinition';
     structureDefinition.kind = this.structureDefinition?.contentType;
     structureDefinition.url = this.structureDefinition?.url;
-    structureDefinition.type = this.structureDefinition.contentFormat === 'json' ? this.structureDefinition?.url : undefined;
+    structureDefinition.type = this.structureDefinition.contentFormat === 'json' ? this.structureDefinition?.code : undefined;
     structureDefinition.parent = this.structureDefinition?.parent;
     structureDefinition.version = this.structureDefinition?.version;
     structureDefinition.fhirVersion = '5.0.0';
