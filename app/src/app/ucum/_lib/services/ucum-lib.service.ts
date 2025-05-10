7842210d6c19f270c6757940bcd09260afe42f24
Mark Porohnja <mark.porohnja@enefit.ee>
2025-05-10 20:08:54 +0300

    apply pagination on client side

--- app/src/app/ucum/_lib/services/ucum-lib.service.ts
+++ app/src/app/ucum/_lib/services/ucum-lib.service.ts
@@ -1,6 +1,6 @@
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
-import {QueryParams, SearchHttpParams, SearchResult} from '@kodality-web/core-util';
+import {SearchHttpParams, SearchResult} from '@kodality-web/core-util';
 import {environment} from 'environments/environment';
 import {map, Observable} from 'rxjs';
 import {Ucum} from '../model/ucum';
@@ -20,36 +20,11 @@ export class UcumLibService {
     return this.http.get<string[]>(`${this.baseUrl}/kinds`);
   }
 
-  // public loadDefinedUnits(): Observable<DefinedUnit[]> {
-  //   return this.http
-  //     .get<{ result: DefinedUnit[] }>(`${this.baseUrl}/defined-units`)
-  //     .pipe(
-  //       map(res => res.result)
-  //     );
-  // }
-
-  public loadDefinedUnitsAsSearchResult(
-    params: QueryParams = new QueryParams()
-  ): Observable<SearchResult<DefinedUnit>> {
+  public loadDefinedUnits(): Observable<DefinedUnit[]> {
     return this.http
       .get<{ result: DefinedUnit[] }>(`${this.baseUrl}/defined-units`)
       .pipe(
-        map(res => {
-          const all = res.result;
-          const total = all.length;
-          const offset = params.offset ?? 0;
-          const limit = params.limit ?? total;
-          const pageItems = all.slice(offset, offset + limit);
-
-          const sr = new SearchResult<DefinedUnit>();
-          sr.data = pageItems;
-          sr.meta = {
-            total,
-            pages: Math.ceil(total / limit),
-            offset
-          };
-          return sr;
-        })
+        map(res => res.result)
       );
   }
 
--- app/src/app/ucum/containers/ucum-list.component.ts
+++ app/src/app/ucum/containers/ucum-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UcumLibService, DefinedUnit } from 'term-web/ucum/_lib';
 import { finalize } from 'rxjs/operators';
-import {QueryParams, SearchResult} from "@kodality-web/core-util";
+import { ComponentStateStore, QueryParams, SearchResult } from '@kodality-web/core-util';
 
 @Component({
   templateUrl: './ucum-list.component.html',
@@ -11,16 +11,50 @@ export class UcumListComponent implements OnInit {
   public searchResult: SearchResult<DefinedUnit> = SearchResult.empty();
   public loading = false;
 
-  public constructor(private ucumSvc: UcumLibService) {}
+  private readonly STORE_KEY = 'ucum-list';
+  private allUnits: DefinedUnit[] = [];
+
+  public constructor(
+    private ucumSvc: UcumLibService,
+    private stateStore: ComponentStateStore
+  ) {}
 
   public ngOnInit(): void {
-    this.loadData();
+    const state = this.stateStore.pop(this.STORE_KEY);
+    if (state?.allUnits) {
+      this.allUnits = state.allUnits;
+      if (state.query) {
+        this.query = state.query;
+      }
+      this.applyQuery();
+    } else {
+      this.loading = true;
+      this.ucumSvc.loadDefinedUnits().pipe(finalize(() => this.loading = false))
+        .subscribe(units => {
+          this.allUnits = units;
+          this.stateStore.put(this.STORE_KEY, { allUnits: units, query: this.query });
+          this.applyQuery();
+        });
+    }
   }
 
   public loadData(): void {
-    this.loading = true;
-    this.ucumSvc.loadDefinedUnitsAsSearchResult(this.query)
-      .pipe(finalize(() => this.loading = false))
-      .subscribe(sr => this.searchResult = sr);
+    this.stateStore.put(this.STORE_KEY, { allUnits: this.allUnits, query: this.query });
+    this.applyQuery();
+  }
+
+  private applyQuery(): void {
+    const offset = this.query.offset ?? 0;
+    const limit = this.query.limit > 0 ? this.query.limit : this.allUnits.length;
+    const page = this.allUnits.slice(offset, offset + limit);
+
+    const sr = new SearchResult<DefinedUnit>();
+    sr.data = page;
+    sr.meta = {
+      total: this.allUnits.length,
+      pages: limit > 0 ? Math.ceil(this.allUnits.length / limit) : 1,
+      offset: offset
+    };
+    this.searchResult = sr;
   }
 }
