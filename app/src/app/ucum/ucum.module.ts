327ec546ffb454a1fa75c32293e4dbdee0bf4200
Mark Porohnja <mark.porohnja@enefit.ee>
2025-05-11 21:19:19 +0300

    unit details view component

--- app/src/app/ucum/ucum.module.ts
+++ app/src/app/ucum/ucum.module.ts
@@ -2,7 +2,6 @@ import {NgModule} from '@angular/core';
 import {Routes} from '@angular/router';
 import {UcumLibModule} from 'term-web/ucum/_lib';
 import {CoreUiModule} from '../core/ui/core-ui.module';
-import {UcumEditComponent} from './containers/edit/ucum-edit.component';
 import {UcumViewComponent} from './containers/edit/ucum-view.component';
 import {UcumMappingListComponent} from './containers/mapping/ucum-mapping-list.component';
 import {UcumListComponent} from './containers/ucum-list.component';
@@ -11,25 +10,21 @@ import {UcumService} from './services/ucum.service';
 
 export const UCUM_ROUTES: Routes = [
   {path: '', component: UcumListComponent},
-  {path: 'add', data: {privilege: ['ucum.CodeSystem.edit']}, component: UcumEditComponent},
-  {path: ':id/edit', data: {privilege: ['ucum.CodeSystem.edit']}, component: UcumEditComponent},
-  {path: ':id/view', component: UcumViewComponent},
+  {path: ':key/view', component: UcumViewComponent},
 ];
 
 @NgModule({
   declarations: [
     UcumListComponent,
-    UcumEditComponent,
     UcumViewComponent,
     UcumMappingListComponent,
   ],
   imports: [
     CoreUiModule,
-    UcumLibModule
+    UcumLibModule,
   ],
   providers: [
     UcumService
   ]
 })
-export class UcumModule {
-}
+export class UcumModule {}
