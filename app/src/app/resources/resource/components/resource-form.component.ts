6a3ded405a8386b244194c95256e6d36b3a7283b
Rainer Randmaa <rainerrandmaa@gmail.com>
2025-10-03 13:22:01 +0300

    proper escaping and formatting of the id pattern

--- app/src/app/resources/resource/components/resource-form.component.ts
+++ app/src/app/resources/resource/components/resource-form.component.ts
@@ -22,7 +22,7 @@ export class ResourceFormComponent implements OnChanges {
   @Input() public resource?: Resource;
   @Input() public mode?: 'add' | 'edit';
 
-  public readonly idPattern: string = '[A-Za-z0-9\\-.]{1,64}';
+  public readonly idPattern: string = '[A-Za-z0-9\\-\\.]{1,64}';
 
   protected loader = new LoadingManager();
   protected customPublisher: boolean = false;
