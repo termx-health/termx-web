e6e8112cce9a5f396674427a31a05f88c5fc3a9f
Igor Bossenko <igor.bossenko@gmail.com>
2026-03-09 18:32:15 +0200

    Update code-system-concepts-list.component.ts

--- app/src/app/resources/code-system/containers/concepts/list/code-system-concepts-list.component.ts
+++ app/src/app/resources/code-system/containers/concepts/list/code-system-concepts-list.component.ts
@@ -14,7 +14,6 @@ import {
 } from 'term-web/resources/_lib';
 import {environment} from 'environments/environment';
 import {debounceTime, forkJoin, fromEvent, map, mergeMap, Observable, of, Subject, takeUntil, tap} from 'rxjs';
-import {debounceTime, forkJoin, fromEvent, map, mergeMap, Observable, of, Subject, takeUntil, tap} from 'rxjs';
 import {ConceptDrawerSearchComponent} from 'term-web/resources/_lib/code-system/containers/concept-drawer-search.component';
 import {ResourceTasksWidgetComponent} from 'term-web/resources/resource/components/resource-tasks-widget.component';
 import {Task} from 'term-web/task/_lib';
@@ -44,7 +43,6 @@ interface ConceptNode {
   expandable?: boolean;
   expanded?: boolean;
   loading?: boolean;
-  detailsExpanded?: boolean;
   detailsExpanded?: boolean;
 
   concept: CodeSystemConcept
@@ -154,12 +152,6 @@ export class CodeSystemConceptsListComponent implements OnInit, AfterViewInit, O
     setTimeout(() => this.refreshAutoOpenedRows(), 1000);
   }
 
-  public ngAfterViewInit(): void {
-    this.treeRows?.changes.pipe(debounceTime(50), takeUntil(this.destroy$)).subscribe(() => this.scheduleAutoOpen());
-    fromEvent(window, 'scroll').pipe(debounceTime(1000), takeUntil(this.destroy$)).subscribe(() => this.refreshAutoOpenedRows());
-    setTimeout(() => this.refreshAutoOpenedRows(), 1000);
-  }
-
   private initConcepts(): void {
     if (this.codeSystem.hierarchyMeaning && !this.codeSystem.settings?.disableHierarchyGrouping) {
       this.groupOpened = true;
@@ -170,8 +162,6 @@ export class CodeSystemConceptsListComponent implements OnInit, AfterViewInit, O
   }
 
   public ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
     this.destroy$.next();
     this.destroy$.complete();
     this.saveState();
@@ -190,10 +180,6 @@ export class CodeSystemConceptsListComponent implements OnInit, AfterViewInit, O
   protected groupConcepts(groupParam: string): void {
     this.loadRootConcepts(groupParam).subscribe(resp => {
       this.rootConcepts = resp.map(c => this.mapToNode(c));
-      if (this.rootConcepts.length > PREVIEW_LOADING_MINIMUM) {
-        this.needsPreview = true;
-        this.rootConcepts.slice(0, INITIAL_FULL_LOAD).forEach(node => node.detailsExpanded = true);
-      }
       if (this.rootConcepts.length > PREVIEW_LOADING_MINIMUM) {
         this.needsPreview = true;
         this.rootConcepts.slice(0, INITIAL_FULL_LOAD).forEach(node => node.detailsExpanded = true);
@@ -300,9 +286,6 @@ export class CodeSystemConceptsListComponent implements OnInit, AfterViewInit, O
         if (this.needsPreview) {
           node.children.slice(0, INITIAL_FULL_LOAD).forEach(child => child.detailsExpanded = true);
         }
-        if (this.needsPreview) {
-          node.children.slice(0, INITIAL_FULL_LOAD).forEach(child => child.detailsExpanded = true);
-        }
         node.expanded = true;
       }).add(() => node.loading = false);
     }
@@ -319,13 +302,10 @@ export class CodeSystemConceptsListComponent implements OnInit, AfterViewInit, O
 
   private mapToNode(c: CodeSystemConcept): ConceptNode {
     const node = {
-    const node = {
       code: c.code,
       expandable: !c.leaf,
       concept: c
     };
-    this.codeToNodeMap.set(node.code, node);
-    return node;
     this.codeToNodeMap.set(node.code, node);
     return node;
   }
@@ -438,34 +418,6 @@ export class CodeSystemConceptsListComponent implements OnInit, AfterViewInit, O
   protected getProperty = (id: number, properties: EntityProperty[]): EntityProperty => {
     return properties?.find(p => p.id === id);
   };
-
-  private scheduleAutoOpen(): void {
-    if (!this.groupOpened) {
-      return;
-    }
-    setTimeout(() => this.refreshAutoOpenedRows(), 0);
-  }
-
-  private refreshAutoOpenedRows(): void {
-    if (!this.groupOpened || !this.treeRows || !this.needsPreview) {
-      return;
-    }
-
-    const rows = this.treeRows.toArray();
-    const visibleCodes = rows
-      .filter(r => {
-        const rect = r.nativeElement.getBoundingClientRect();
-        return rect.bottom >= 0 && rect.top <= window.innerHeight;
-      })
-      .map(r => r.nativeElement.getAttribute('data-code'))
-      .filter((code): code is string => !!code);
-
-    console.log("Visible code length: ", visibleCodes.length);
-    visibleCodes.forEach(code => {
-      const node = this.codeToNodeMap.get(code);
-      node.detailsExpanded = true;
-    });
-  }
 
   private scheduleAutoOpen(): void {
     if (!this.groupOpened) {
